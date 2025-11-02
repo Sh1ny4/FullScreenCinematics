14944cbd41650a4837cd8386359b2540f618d7c4
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2025-11-02 17:30:45 +0100

    Initial commit

---
+++ FullScreenCinematics/FullScreenCinematics/SubModule.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace FullScreenCinematics
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            new Harmony("FullScreenCinematics.patches").PatchAll();
+        }
+    }
+}
